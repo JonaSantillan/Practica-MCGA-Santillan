7445aab7545a6ce661b72b4187614aa6a35766a8
Jonatan Santillan <jonatansantillan@gmail.com>
2020-08-31 17:08:56 -0300

    Versión 2

--- Ejercicio 1/src/user/user.js
+++ Ejercicio 1/src/user/user.js
@@ -1,33 +1,49 @@
 import React from 'react'
 import css from './user.module.css'
 
-const User = (props) => {
-    return(
+class User extends React.Component {
+
+    state = {
+      open: false,
+    }
+  
+    showInfo = () => {
+      this.setState({
+        open: !this.state.open,
+      })
+    }
+
+ render = () => {
+    return (
         <div className={css.contenedor}>
             <h1 className={css.name}>
-                {props.name}
+                {this.props.name}
             </h1>
-            <div className={css.info}>
-                <div className={css.item}>
-                    <span className={css.key}>Nombre: </span>
-                    <span className={css.valor}>{props.username}</span>
-                </div>
-                <div className={css.item}>
-                    <span className={css.key}>Teléfono: </span>
-                    <span className={css.valor}>{props.phone}</span>
-                </div>
-                <div className={css.item}>
-                    <span className={css.key}>e-mail: </span>
-                    <span className={css.valor}>{props.email}</span>
-                </div>
-            </div>
+            <button className={css.button} onClick={this.showInfo}>Más Info +</button>
+            {this.state.open
+                && (
+                <div className={css.info}>
+                    <div className={css.item}>
+                        <span className={css.key}>Nombre: </span>
+                        <span className={css.valor}>{this.props.username}</span>
+                    </div>
+                    <div className={css.item}>
+                        <span className={css.key}>Teléfono: </span>
+                        <span className={css.valor}>{this.props.phone}</span>
+                    </div>
+                    <div className={css.item}>
+                        <span className={css.key}>e-mail: </span>
+                        <span className={css.valor}>{this.props.email}</span>
+                    </div>
+                </div>)}
             <button className={css.boton} onClick={() => {
-                props.removeUser(props.id)
+                this.props.removeUser(this.props.id)
             }}>
                 Eliminar Usuario
             </button>
         </div>
-    )
+        )
+    }
 }
 
 export default User
