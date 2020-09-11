f610347523a4d87642dbe92dcf5a7b5372fcd508
Jonatan Santillan <jonatansantillan@gmail.com>
2020-09-10 21:25:59 -0300

    Mejoras

--- tp_1_jonatan_santillan/src/Vista/vista.js
+++ tp_1_jonatan_santillan/src/Vista/vista.js
@@ -1,41 +1,25 @@
 import React from 'react'
 import Css from './vista.module.css'
 
-class Vista extends React.Component {
 
-    state = {
-        numero : 0,
-        lista_contadores : []
-      }
+class Contador extends React.Component {
 
     render = () => {
         return(
-            <div className={Css.cont_contadores}>
-                <div className={Css.texto3}>Lista de Contadores</div>
-                <div className={Css.cont_contador}>
-                    <button className={Css.botonmasmenos} onClick={() => {
-                        if(this.state.numero >= 0){
-                            this.state.numero = this.state.numero + 1
-                            this.setState({
-                                numero : this.state.numero
-                            })
-                        }
-                    }}>+</button>
-                    <div className={Css.numero}>{this.state.numero}</div>
-                    <button className={Css.botonmasmenos} onClick={() => {
-                        if(this.state.numero >= 0){
-                            this.state.numero = this.state.numero - 1
-                            this.setState({
-                                numero : this.state.numero
-                            })
-                        }
-                    }}>-</button>
-                    <button className={Css.botoneliminar}>Eliminar</button>
-                </div>
+            <div className={Css.cont_contador}>
+                <button className={Css.botonmasmenos} onClick={(props) => {
+                    this.props.sumar(this.props.index)
+                }}>+</button>
+                <div className={Css.numero}>{this.props.numero}</div>
+                <button className={Css.botonmasmenos} onClick={(props) => {
+                    this.props.restar(this.props.index)
+                }}>-</button>
+                <button className={Css.botoneliminar} onClick={(props) => {
+                    this.props.eliminar(this.props.index)
+                }}>Eliminar</button>
             </div>
-
         )
-    }
+    }   
 }
 
-export default Vista
+export default Contador
--- tp_1_jonatan_santillan/src/app.js
+++ tp_1_jonatan_santillan/src/app.js
@@ -1,8 +1,61 @@
 import React from 'react';
 import Css from './app.module.css';
-import Vista from './Vista/vista'
+import Contador from './Vista/vista' 
+
 
 class App extends React.Component {
+
+  state = {
+    numero : 0,
+    data : []
+  }
+
+  agregar = () => {
+      let data = this.state.data
+      let newdata = [...data, 0]
+      this.setState({
+          data : newdata
+      })
+  }
+
+  eliminar = (index) => {
+    const newlista = this.state.data.filter((item, i) => {
+      if(index !== i){
+        return true
+      }
+      return false
+    })
+
+    this.setState({
+      data : newlista
+    })
+  }
+
+  sumar = (index) => {
+    const newlista = this.state.data.map((item, i) =>{
+      if(index === i){
+        return item + 1
+      }
+      return item
+    })
+    this.setState({
+      data : newlista
+    })
+  }
+
+  restar = (index) => {
+    const newlista = this.state.data.map((item, i) =>{
+      if(index === i){
+        return item - 1
+      }
+      return item
+    })
+    this.setState({
+      data : newlista
+    })
+  }
+  
+
   render (){
     return(
       <div className={Css.app}>
@@ -14,10 +67,24 @@ class App extends React.Component {
           <div className={Css.cont_boton}>
               <div className={Css.texto}>Alumno</div>
               <div className={Css.texto2}>Jonatan Santillan</div>
-              <button className={Css.boton}>Agregar Contador</button>
+              <button className={Css.boton} onClick={this.agregar}>Agregar Contador</button>
           </div>
           
-          <Vista />
+          <div className={Css.cont_contadores}>
+            <div className={Css.texto3}>Lista de Contadores</div>
+              {this.state.data.map ((item, index) =>{
+                return(
+                  <Contador 
+                    key = {index}
+                    index = {index}
+                    numero = {item}
+                    eliminar = {this.eliminar}
+                    sumar = {this.sumar}
+                    restar = {this.restar}
+                  />
+                )
+              })}
+          </div>
           
         </div>
       </div>
